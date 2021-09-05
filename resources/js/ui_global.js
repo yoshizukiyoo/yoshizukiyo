a36cf5cc0b345976744d696588db72b1c6dc243a
임미선 <leroro@inseq.co.kr>
2021-09-06 07:30:51 +0900

    #56 [공통] UI 관련 스크립트 파일 분기

---
+++ resources/js/ui_global.js
@@ -0,0 +1,31 @@
+// ui_global.js : 필수 스크립트 정의
+
+// IE 감지
+var agent = navigator.userAgent.toLowerCase();
+if (agent.indexOf("msie") > -1 || agent.indexOf("trident") > -1) {
+	$('body').addClass('ie');
+}
+
+// Skip Nav
+$(document).on('click', '.skip_nav', function (e) {
+	e.preventDefault();
+	var tg = $(this).attr('href');
+	$(tg).attr('tabindex', '0').focus();
+});
+
+// 더보기 박스
+$(function () {
+	$('.box_more').each(function () {
+		$(this).wrapInner('<div class="inner"></div>').append('<button type="button" class="btn btn_more_white">더보기</button>');
+	});
+});
+
+$(document).on('click', '.box_more .btn_more_white', function () {
+	if ($(this).closest(".box_more").hasClass("new_controlls")) {
+		$(this).parent('.box_more').hasClass('opened') ? $(this).text('계약정보 상세보기') : $(this).text('상세보기 닫기');
+		$(this).parent('.box_more').toggleClass('opened');
+	} else {
+		$(this).parent('.box_more').hasClass('opened') ? $(this).text('더보기') : $(this).text('접기');
+		$(this).parent('.box_more').toggleClass('opened');
+	}
+});
