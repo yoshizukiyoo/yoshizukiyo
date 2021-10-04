578331ca5911aa728bb68506ec84ae6989c367ef
임미선 <leroro@inseq.co.kr>
2021-10-05 04:27:06 +0900

    탭메뉴 ing

--- resources/js/ui_layout_setting.js
+++ resources/js/ui_layout_setting.js
@@ -56,94 +56,78 @@ $(function () {
 	});
 });
 
-// 게시판 등록 : 파일첨부
-$(function () {
-	var fileTarget = $('.form_file');
-	fileTarget.on('change', function () {
-		if (window.FileReader) {
-			var filename = $(this)[0].files[0].name;
-		} else {
-			var filename = $(this).val().split('/').pop().split('\\').pop();
-		}
-		$(this).next('.file-name').val(filename);
-	});
-
-	$('.file-attatch-item .file-name').on('click', function () {
-		$(this).prev('.form_file').click();
-	});
-});
 
 // 데이트피커
-setTimeout(function () {
-	$('.tf_datepicker').each(function () {
-		setDatepicker($(this));
-	});
-	$(document).on('click', '.tf_datepicker', function () {
-		// $('body').append('<div class="modal_popup modal_bottom_sheet hide" data-popup="layerDatepicker"><div class="dimed remove"></div><div class="popup_inner"><div class="popup_header"><h2 class="popup_tit">날짜 선택</h2></div><div class="popup_body"></div><button type="button" class="btn btn_close_popup remove">닫기</button></div></div>');
-		// $('body').append('<div class="modal_popup modal_bottom_sheet hide" data-popup="layerDatepicker"><div class="dimed remove"></div><div class="popup_inner"><div class="popup_body"></div></div></div>');
-		openCalendar($(this));
-		// $('#inseq-datepicker').appendTo('.modal_popup[data-popup=layerDatepicker] .popup_body');
-		// openModal('layerDatepicker');
-	});
-}, 200);
+// setTimeout(function () {
+// 	$('.tf_datepicker').each(function () {
+// 		setDatepicker($(this));
+// 	});
+// 	$(document).on('click', '.tf_datepicker', function () {
+// 		// $('body').append('<div class="modal_popup modal_bottom_sheet hide" data-popup="layerDatepicker"><div class="dimed remove"></div><div class="popup_inner"><div class="popup_header"><h2 class="popup_tit">날짜 선택</h2></div><div class="popup_body"></div><button type="button" class="btn btn_close_popup remove">닫기</button></div></div>');
+// 		// $('body').append('<div class="modal_popup modal_bottom_sheet hide" data-popup="layerDatepicker"><div class="dimed remove"></div><div class="popup_inner"><div class="popup_body"></div></div></div>');
+// 		openCalendar($(this));
+// 		// $('#inseq-datepicker').appendTo('.modal_popup[data-popup=layerDatepicker] .popup_body');
+// 		// openModal('layerDatepicker');
+// 	});
+// }, 200);
 
-function removeDatepicker() {
-	if ($('#inseq-datepicker').length > 0) {
-		$('#inseq-datepicker').remove();
-	}
-}
+// function removeDatepicker() {
+// 	if ($('#inseq-datepicker').length > 0) {
+// 		$('#inseq-datepicker').remove();
+// 	}
+// }
 
-$(document).on('click', '.btn_datepicker', function (e) {
-	e.preventDefault();
-	$(this).closest('.tf_item').find('.tf_datepicker').click();
-});
+// $(document).on('click', '.btn_datepicker', function (e) {
+// 	e.preventDefault();
+// 	$(this).closest('.tf_item').find('.tf_datepicker').click();
+// });
 
-function setDatepicker(obj) {
-	$(obj).closest('.tf_item').append('<div class="btn_area"><button type="button" class="btn btn_icon btn_datepicker"><i class="ico ico_datepicker"></i><span class="sr_only">달력</span></button></div>');
-}
+// function setDatepicker(obj) {
+// 	$(obj).closest('.tf_item').append('<div class="btn_area"><button type="button" class="btn btn_icon btn_datepicker"><i class="ico ico_datepicker"></i><span class="sr_only">달력</span></button></div>');
+// }
 
 // 일반 레이어
-$(function () {
-	$('.layer_popup.show').each(function () {
-		openLayer($(this), null);
-	});
-});
+// $(function () {
+// 	$('.layer_popup.show').each(function () {
+// 		openLayer($(this), null);
+// 	});
+// });
 
-function openLayer(_target, _opener) {
-	if ($(_target).length > 0) {
-		layerOpener = _opener;
-		setTimeout(function () {
-			$(_target).addClass('show').removeClass('hide').css({
-				left: $(_opener).position().left,
-				top: $(_opener).outerHeight() + $(_opener).position().top,
-			});
-		}, 100);
-		setTimeout(function () {
-			$('.popup_inner', _target).attr('tabindex', '0').focus();
-			$(_target).scrollTop(0);
-		}, 300);
-	}
-}
+// function openLayer(_target, _opener) {
+// 	if ($(_target).length > 0) {
+// 		layerOpener = _opener;
+// 		setTimeout(function () {
+// 			$(_target).addClass('show').removeClass('hide').css({
+// 				left: $(_opener).position().left,
+// 				top: $(_opener).outerHeight() + $(_opener).position().top,
+// 			});
+// 		}, 100);
+// 		setTimeout(function () {
+// 			$('.popup_inner', _target).attr('tabindex', '0').focus();
+// 			$(_target).scrollTop(0);
+// 		}, 300);
+// 	}
+// }
 
-function closeLayer(_target, _opener) {
-	var tg = $(_target);
-	if (tg.hasClass('show')) {
-		if ($('.layer_popup.show').length == 1) {}
-		tg.addClass('hide').removeClass('show');
-		var modalOpener = $(_opener);
-		if (modalOpener.length) {
-			modalOpener.focus();
-		}
-	} else {
-		alert('닫을 레이어를 올바로 지정해 주세요. \n closeLayer(\'#레이어아이디\')')
-	}
-}
+// function closeLayer(_target, _opener) {
+// 	var tg = $(_target);
+// 	if (tg.hasClass('show')) {
+// 		if ($('.layer_popup.show').length == 1) {}
+// 		tg.addClass('hide').removeClass('show');
+// 		var modalOpener = $(_opener);
+// 		if (modalOpener.length) {
+// 			modalOpener.focus();
+// 		}
+// 	} else {
+// 		alert('닫을 레이어를 올바로 지정해 주세요. \n closeLayer(\'#레이어아이디\')')
+// 	}
+// }
 
-var layerOpener = null;
-$(document).on('click', '.layer_popup .btn_close_popup', function () {
-	var target = $(this).closest('.layer_popup').attr('id');
-	closeModal('#' + target, layerOpener);
-});
+// var layerOpener = null;
+// $(document).on('click', '.layer_popup .btn_close_popup', function () {
+// 	var target = $(this).closest('.layer_popup').attr('id');
+// 	closeModal('#' + target, layerOpener);
+// });
 
 // 모달 레이어
 $(function () {
@@ -252,98 +236,39 @@ function bodyScroll(_status, _orgWidth) {
 	}
 }
 
-// 윈도우 팝업 오픈
-var popOpenBtn = null;
-
-function openWindow(_obj, popName, w, h, _opener) {
-	var popW = 900;
-	var popH = 600;
-	var myUrl = _obj;
-
-	if (typeof _obj !== 'string') {
-		if ($(_obj).prop('tagName') === 'A') {
-			popOpenBtn = $(_obj);
-			myUrl = $(_obj).attr('href');
-		} else if (_opener) {
-			popOpenBtn = $(_opener);
+// 탭메뉴
+$(function () {
+	$('.tab_list a').on('click', function (e) {
+		var tg = $(this).attr('href');
+		if (!$(this).closest('.tab_menu').hasClass('tab_not_active')) {
+			$(this).parent('li').addClass('on').siblings('li').removeClass('on');
 		}
-	}
+		if (tg === '#' || tg === '' || tg === '#;') {
+			e.preventDefault();
+		} else if (tg.charAt(0) === '#') {
+			if ($(tg).hasClass('tab_cont')) {
+				$(tg).show().siblings('.tab_cont').hide();
+				e.preventDefault();
+			}
+		}
+	});
 
-	if (w) popW = w;
-	if (h) popH = h;
-	var left = window.screenX + (window.outerWidth - popW) / 2;
-	var top = window.screenY + (window.outerHeight - popH) / 2;
-	window.open(myUrl, popName, 'width=' + popW + ', height=' + popH + ', left=' + left + ', top=' + top + ', scrollbars=yes');
-}
-
-function closeWindow() {
-	if (window.opener != null) {
-		window.opener.openerFocus();
-	}
-	window.close();
-}
-
-function openerFocus() {
-	if (popOpenBtn != null) {
-		$(popOpenBtn).focus();
-		popOpenBtn = null;
-	}
-}
-
-// 더블 탭메뉴 활성화
-$(document).on('click', '.tab_menu_type3 a , .tab_menu_double > .tab_menu_type1 a, .tab_btn a ', function () {
-	var obj = $(this);
-	var tg = obj.data('tab-tg');
-	obj.addClass('current').parent().siblings().find('>a').removeClass();
-	$('[data-sub-tab^="sub"]').hide();
-	$('[data-sub-tab=' + tg + ']').show();
-});
-
-// 디자인 셀렉트박스
-$(document).on('click', '.selectbox_wrap dt a', function () {
-	var obj = $(this);
-	$('.selectbox_wrap').not(obj.parents('.selectbox_wrap')).removeClass('active');
-	obj.parents('.selectbox_wrap').toggleClass('active');
-	return false;
-});
-$(document).on('click', '.selectbox_wrap dd a', function () {
-	var obj = $(this);
-	obj.closest('dd').prev().find('>a>span').text(obj.text());
-	obj.parents('.selectbox_wrap').toggleClass('active').removeClass('error');
-	obj.parents('.selectbox_wrap').closest('td').find('> .error_info').hide();
-	return false;
-});
-var win = $(window);
-var selectBox = $('.selectbox_wrap dt a');
-win.on("click", function (e) {
-	if (selectBox.has(e.target).length == 0 && !selectBox.is(e.target)) {
-		$('.selectbox_wrap').removeClass('active');
-	}
+	$('.tab_menu1').each(function () {
+		var containerWidth = $(this).width();
+		var innerWidth = 0;
+		var current = $('li.on', this).index();
+		$('li', this).each(function () {
+			innerWidth += $(this).width();
+		});
+		if (containerWidth < innerWidth) {
+			$(this).addClass('scroll_enable');
+		}
+		// $(this).addClass('scroll_enable');
+		var posL = $('li', this).eq(current - 1).position().left;
+		$(this).children().scrollLeft(posL);
+	});
 });
 
 // 
 // 레이아웃
 //
-
-// Breadcrumb
-$(document).on('click', '.breadcrumb_list>li>a', function (e) {
-	if ($(this).next('.sub_list_wrap').length) {
-		e.preventDefault();
-		$(this).parent('li').toggleClass('on').siblings('li').removeClass('on');
-	}
-}).on('keydown', '.breadcrumb_list ul>li:last-child>a', function (e) {
-	if (e.keyCode == 9 && !e.shiftKey) {
-		$('.breadcrumb_list>li').removeClass('on');
-	}
-});
-$(document).on('mousedown', function (e) {
-	var $obj = $('.breadcrumb_list');
-	if ($obj.length > 0) {
-		var objPos = $obj.offset();
-		objPos.right = (objPos.left + $obj.width());
-		objPos.bottom = (objPos.top + $obj.height());
-		if ((e.pageX < objPos.left || e.pageX > objPos.right || e.pageY < objPos.top || e.pageY > objPos.bottom) && !$(e.target).closest('.breadcrumb_list').length) {
-			$('.breadcrumb_list>li').removeClass('on');
-		}
-	}
-});
