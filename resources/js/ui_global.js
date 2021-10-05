3fe1a6479ac967a6569c95b58ca2a807cd2700d7
임미선 <leroro@inseq.co.kr>
2021-10-05 17:32:13 +0900

    스크롤 테이블 예제 추가

--- resources/js/ui_global.js
+++ resources/js/ui_global.js
@@ -22,3 +22,34 @@ $(function () {
 		$('[data-toggle-content=' + toggleContent + ']').toggle();
 	});
 });
+
+// 테이블 스크롤 표시
+$(document).ready(function () {
+	if ($('.scroll_track').length > 0) {
+		tableScroll();
+	}
+});
+
+function tableScroll() {
+	var table = $('.scroll_track');
+
+	table.each(function () {
+		var $this = $(this),
+			ingClass = 'scroll_ing',
+			endClass = 'scroll_end';
+		$this.on('scroll', function (e) {
+			var wrap = $(this).closest('.tbl'),
+				currLeft = this.scrollLeft,
+				scrWidth = this.scrollWidth,
+				cliWidth = this.clientWidth;
+
+			if (currLeft === 0) {
+				wrap.removeClass(ingClass).removeClass(endClass);
+			} else if (currLeft !== 0 && currLeft + cliWidth < scrWidth) {
+				wrap.addClass(ingClass).removeClass(endClass);
+			} else {
+				wrap.addClass(endClass);
+			}
+		});
+	});
+}
